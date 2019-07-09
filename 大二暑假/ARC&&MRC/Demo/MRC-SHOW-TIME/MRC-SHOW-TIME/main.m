7be62f456979c3e74c2e7fa50b020c6be439a828
KevinAshen <894912881@qq.com>
2019-07-09 22:27:55 +0800

    ARC
    好累呀

--- 大二暑假/ARC&&MRC/Demo/MRC-SHOW-TIME/MRC-SHOW-TIME/main.m
+++ 大二暑假/ARC&&MRC/Demo/MRC-SHOW-TIME/MRC-SHOW-TIME/main.m
@@ -15,22 +15,69 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+    
+//        NSArray *arr = [NSArray array];
+//        NSLog(@"%ld", arr.retainCount);
         
         NSObject *obj = [[NSObject alloc] init];
         //printf("retain count = %ld\n", obj.retainCount);
         [Print printRetainCount:obj];
+
+        NSLog(@"%p", obj);
+        
+        //[obj release];
+        
+        [obj dealloc];
+        
+        NSLog(@"%p", obj);
+        
+        //[obj release];
+        
+        NSLog(@"%p", obj);
         //在MRC时代，创建对象时，会把引用计数自动设成1，也就是说，创建时两者其实是一致的，都是引用计数从0变成1
 //        [obj retain];
 //
 //        [Print printRetainCount:obj];
         
-        [obj release];
+       // [obj release];
         
-        [Print printRetainCount:obj];
+        //id array = [NSMutableArray arrayWithCapacity:1];
         
-        [obj release];
+//        [obj retain];
+//        [obj release];
+//        [obj retain];
+//        [obj retain];
+//        [Print printRetainCount:obj];
+//        [Print printRetainCount:obj];
+        
+        
+        
+    
+        
+        //[obj dealloc];
         
         //[Print printRetainCount:obj];
+        
+        
+        //NSLog(@"%ld", obj.retainCount);
+        
+       // NSLog(@"%p", obj);
+        
+        //[Print printRetainCount:obj];
+        
+//        [obj release];
+//
+//        [Print printRetainCount:obj];
+        
+        //[obj retain];
+        
+        //[Print printRetainCount:obj];//这里和书上描述的一致，我们进行alloc，计数是1，进行release操作，发现打印还是1，
+        
+        //[obj release];
+        
+        //[Print printRetainCount:obj];
+        
+        //[obj retain];
         
         //[Print printRetainCount:obj];
         
